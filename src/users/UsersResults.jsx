5f02aff27ee12b73a1fd9fc577a79606740b318b
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-14 10:20:00 +0530

    updations of files has been done

--- src/users/UsersResults.jsx
+++ src/users/UsersResults.jsx
@@ -2,22 +2,22 @@ import React, { useEffect } from 'react'
 
 function UsersResults() {
 
-    // useEffect(()=>{
-    //     fetchUser()
-    // },[])
+    useEffect(()=>{
+        fetchUser()
+    },[])
 
-    // const fetchUser = async()=>{
-    //  const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
-    //     headers:{
-    //         Authorization : `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    //     },
-    //  })
-    //  const data = await response.json();
-    //  console.log(data);
-    // }
+    const fetchUser = async()=>{
+     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
+        headers:{
+            Authorization : `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+     })
+     const data = await response.json();
+     console.log(data);
+    }
   return (
     <div>
-      its user result {process.env.REACT_APP_GITHUB_URL}SDFDS
+      its user result {process.env.REACT_APP_GITHUB_TOKEN}
     </div>
   )
 }
