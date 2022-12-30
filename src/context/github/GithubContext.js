c40998c517cf9e48c1177ebb2b4df0821fcda9d6
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-30 12:03:58 +0530

    fetching single user has been done

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({children}) =>{
 
     const initialState = {
         users:[],
+        user:{},
         loading:false
     }
 
@@ -35,6 +36,28 @@ export const GithubProvider = ({children}) =>{
         })
        }
 
+    //    Get single user!...
+       const getUser = async(login)=>{
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`,{
+           headers:{
+               Authorization : `token ${GITHUB_TOKEN}`,
+           },
+        })
+
+        if(response.status === 404){
+            window.location='/notfound'
+        }
+        else{
+            const date = await response.json();
+            dispatch({
+                type:'GET_USER',
+                payload:date,
+            })
+        }
+        
+       }
     // Clear users from state
     const clearUsers = () =>{
         dispatch({
@@ -49,8 +72,10 @@ export const GithubProvider = ({children}) =>{
     return <GithubContext.Provider value={{
         users:state.users,
         loading:state.loading,
+        user:state.user,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser,
     }}>
         {children}
     </GithubContext.Provider>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import GithubContext from '../context/github/GithubContext';
 
 function User() {
+
+  const {getUser,user} = useContext(GithubContext);
+  const params = useParams()
+
+  useEffect(()=>{
+    getUser(params.login)
+  },[])
+
   return (
     <div>
-      User
+      {user.login}
     </div>
   )
 }
