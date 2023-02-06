2a99e1f1d80ce8e128cf7e6b68bb43f2aa96758d
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-02-06 08:27:25 +0530

    code cleanup has been done

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,32 +1,30 @@
-const githubReducer = (state, action) =>{
-    switch(action.type){
-        case 'GET_USERS':
-            console.log(action.payload);
-            return{
-                
-                ...state,
-                users:action.payload,
-                loading:false
-            }
-        case 'GET_USER_AND_REPOS':
-            return{
-                ...state,
-                user : action.payload.user,
-                repos : action.payload.repos,
-                loading:false
-            }
-        case 'SET_LOADING':
-            return{
-                ...state,
-                loading:true
-            }
-        case 'CLEAR_USERS':
-            return{
-                ...state,
-                users : []
-            }
-        default:
-          return  state
-    }
-}
-export default githubReducer
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case "GET_USER_AND_REPOS":
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
+      };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
+    default:
+      return state;
+  }
+};
+export default githubReducer;
